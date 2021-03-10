1fcb1b34ff5f2714f5d2799032fa477c7cacd12d
linjiev5 <linjiev5@yahoo.co.jp>
2021-03-10 18:26:41 +0900

    Initial commit

---
+++ src/test/java/jp/easyrecrui/EasyRecruiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.easyrecrui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRecruiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
